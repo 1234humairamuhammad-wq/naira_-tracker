4b77674c6045f2835e1bd9292e8657a0457520b1
1234humairamuhammad-wq <1234humairamuhammad@gmail.com>
2026-08-27 21:01:49 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ if 'balance' not in st.session_state:
 if 'history' not in st.session_state:
     st.session_state.history = []
 
-st.set_page_config(page_title="My Naira Tracker", page_icon="₦")
+st.set_page_config(page_title="My Naira Tracker")
 st.title("₦ My Naira Tracker")
 st.header(f"Current Balance: ₦{st.session_state.balance:,}")
 
@@ -13,8 +13,8 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.subheader("Add Income")
-    income_amount = st.number_input("Amount", min_value=1, key="income")
-    income_cat = st.text_input("Category", "Salary", key="inc_cat")
+    income_amount = st.number_input("Amount", min_value=0, step=1000)
+    income_cat = st.text_input("Category", "Salary")
     if st.button("➕ Add Income"):
         st.session_state.balance += income_amount
         st.session_state.history.append(f"+ ₦{income_amount:,} - {income_cat}")
@@ -23,14 +23,14 @@ with col1:
 
 with col2:
     st.subheader("Add Expense")
-    expense_amount = st.number_input("Amount", min_value=1, key="expense")
-    expense_cat = st.text_input("Category", "Food", key="exp_cat")
+    expense_amount = st.number_input("Amount", min_value=0, step=1000)
+    expense_cat = st.text_input("Category", "Food")
     if st.button("➖ Add Expense"):
         st.session_state.balance -= expense_amount
         st.session_state.history.append(f"- ₦{expense_amount:,} - {expense_cat}")
         st.error(f"Spent ₦{expense_amount:,}!")
         st.rerun()
 
-st.subheader("📜 Last 10 Transactions")
+st.subheader("📒 Last 10 Transactions")
 for item in st.session_state.history[-10:]:
     st.write(item)
